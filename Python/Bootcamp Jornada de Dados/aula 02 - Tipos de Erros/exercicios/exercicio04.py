408b0aaa5499983d961b1c751564a2d563cf71ce
Leandro Amoras <amorasleandro@gmail.com>
2025-05-22 11:52:21 -0300

    feat: add int exercises from aula 02

---
+++ Python/Bootcamp Jornada de Dados/aula 02 - Tipos de Erros/exercicios/exercicio04.py
@@ -0,0 +1,7 @@
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+primeiro_numero = int(input("Digite um número inteiro: "))
+segundo_numero = int(input("Digite outro número inteiro: "))
+
+divisao = primeiro_numero // segundo_numero
+
+print(f"A divisão entre os números {primeiro_numero} e {segundo_numero}, é: {divisao}")
---
+++ Python/Bootcamp Jornada de Dados/aula 02 - Tipos de Erros/exercicios/exercicio05.py
@@ -0,0 +1,6 @@
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+numero = int(input("Digite um número inteiro: "))
+
+quadrado = numero **2
+
+print(f"O número {numero} elevado ao quadrado, é: {quadrado}")
