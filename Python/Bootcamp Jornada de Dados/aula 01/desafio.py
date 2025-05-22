ffba89af5f10b4ec2584203d2e8a9ebd88dce88e
Leandro Amoras <amorasleandro@gmail.com>
2025-05-22 10:40:42 -0300

    feat: finish desafio.py

--- Python/Bootcamp Jornada de Dados/aula 01/desafio.py
+++ Python/Bootcamp Jornada de Dados/aula 01/desafio.py
@@ -1,15 +1,21 @@
 # 1) Solicite ao usuario que digite seu nome
+name = str(input("Digite seu nome (Ex: Leandro): "))
 
 # 2) Solicete ao usuario que digite o valor do seu salario
 # (Converta o valor de entrada para float)
+salario = float(input("Digite o valor do seu salario (Ex: 1000): "))
 
-# 3) Solicite ao usuario que digite o valor do bonus recebido
+# 3) Solicite ao usuario que digite a porcentagem do bonus recebido
 # (Converta o valor de entrada para float)
+bonus = float(input("Digite a porcentagem do seu bonus (Ex: 2.5): "))
 
 # 4) Calcule o valor do bonus final
+kpi = 1000+salario*bonus
 
 # 5) Imprima o valor do KPI para o usuario
+print(kpi)
 
 # 6) Imprima uma mensagem personalizada incluindo o nome, salario e bonus do usuario
+print(f"Olá {name}, o seu bonus foi de {kpi}")
 
 # Bonus: Quantos bugs voce vai identificar nesse programa?
